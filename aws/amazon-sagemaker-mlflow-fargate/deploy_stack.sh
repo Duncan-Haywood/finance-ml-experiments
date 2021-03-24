d64834c6fb1a6a8f620c90e184e09bf3e3a6e13a
Duncan Haywood <duncanhaywood@appliedmlexperts.com>
2021-03-24 16:59:53 +0000

    adds docker requirements

--- aws/amazon-sagemaker-mlflow-fargate/deploy_stack.sh
+++ aws/amazon-sagemaker-mlflow-fargate/deploy_stack.sh
@@ -5,6 +5,9 @@ source .venv/bin/activate
 pip3 install -r requirements.txt
 npm install -g aws-cdk@1.65.0
 aws configure
+sudo yum update -y
+sudo yum install docker -y
+sudo service docker start
 ## needs to run aws configure and set up IAM user with several access policies before the following will work; needs docker as well and potentially needs to be outside sagemaker studio
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account | tr -d '"')
 AWS_REGION=$(aws configure get region)
