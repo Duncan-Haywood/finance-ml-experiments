e33ffc17c7f872f0ce4b6fcb7e4209d1461fad06
Duncan Haywood <duncanhaywoodacademic@gmail.com>
2021-05-20 21:52:59 -0500

    moves file; begins change to full python - no bash

--- aws/production_experiment/preprocess/build_container.py
+++ aws/production_experiment/preprocess/build_container.py
@@ -1,6 +1,5 @@
 import boto3
-import os
-
+import docker
 
 def main():
     account_id = boto3.client('sts').get_caller_identity().get('Account')
@@ -9,14 +8,15 @@ def main():
     tag = ':latest'
     processing_repository_uri = '{}.dkr.ecr.{}.amazonaws.com/{}'.format(
         account_id, region, ecr_repository + tag)
+    ecr_client = boto3.client('ecr')
     # Create ECR repository and push docker image
-    os.system('''
-    docker build -t $ecr_repository docker
+    # f'docker build -t {ecr_repository} docker'.split()
+    """
     aws ecr get-login-password --region {region} | docker login --username AWS --password-stdin {account_id}.dkr.ecr.{region}.amazonaws.com
     aws ecr create-repository --repository-name $ecr_repository
     docker tag {ecr_repository + tag} $processing_repository_uri
     docker push $processing_repository_uri
-    ''')
+    """
 
 
 if __name__ == '__main__':
