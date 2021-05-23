5d12f3fe7051b33cbeb568cf7f68bdc193aba913
Duncan-Haywood <duncanhaywood@appliedmlexperts.com>
2021-05-23 18:28:58 -0500

    adds test skeleton for build contatiner

---
+++ aws/production_experiment/preprocess/test_build_contatiner.py
@@ -0,0 +1,28 @@
+class TestConstants:
+    def test_get_account_id(self):
+        assert 0
+    def test_region_id(self):
+        assert 0
+class TestBuildContatiner:
+    def test_ecr_client_connection(self):
+        assert 0
+    def test_docker_client_connection(self):
+        assert 0
+    def test_build(self):
+        assert 0
+    def test_repository_create(self):
+        assert 0
+class TestLoginAuthentication:
+    def authentication_received(self):
+        assert 0
+    def test_auth_username_valid(self):
+        assert 0
+    def test_docker_login(self):
+        assert 0
+class TestDockerTagPush:
+    def test_docker_tag(self):
+        assert 0
+    def test_docker_push(self):
+        assert 0
+
+    
